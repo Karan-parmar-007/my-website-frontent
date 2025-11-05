03e987b0caf7ca13f243a3888e3fba779f14e1db
Karan-parmar-007 <karanparmar00014@gmail.com>
2025-11-05 07:12:22 +0000

    env check

--- src/lib/users_apis.js
+++ src/lib/users_apis.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log(`API_BASE_URL: ${API_BASE_URL}`);
 const api = axios.create({
   baseURL: `${API_BASE_URL}/v1`,
   withCredentials: true,
