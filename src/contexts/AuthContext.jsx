a7d2b874d15f391c60366bb83706e25e989d3fcd
Karan-parmar-007 <karanparmar00014@gmail.com>
2025-11-05 15:14:31 +0000

    updated the way to fetch apis

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { loginUser, registerUser, getCurrentUser, logoutUser } from '../lib/user_apis';
 
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
